cef76af608aed0ca8e7e185576f39784bdce4fdc
Huh521 <huhgoo99@gmail.com>
2021-05-19 19:21:09 +0900

    Initial commit

---
+++ app/src/main/java/org/hhw/project_with_a_jump/MainActivity.java
@@ -0,0 +1,14 @@
+package org.hhw.project_with_a_jump;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
