597ec5db95935dc9e8e9bd487b56b07f49c52c0b
willy019@naver.com <84445381+yeji1008@users.noreply.github.com>
2021-06-27 18:19:51 +0900

    사용자화면 - 출입명부 이름, 개인 안심번호, 현재 위치 -> 정보 가지고 오기 출입명부 DB에 저장 toolbar을 이용해 홈화면으로 가도록 변경하기

--- app/src/main/java/com/application/helloandroid/MainActivity.java
+++ app/src/main/java/com/application/helloandroid/MainActivity.java
@@ -5,28 +5,44 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    // "인증하기" 버튼
     Button button1;
+    TextView Day;
+
+    //현재 시간
+    long now = System.currentTimeMillis();
+    Date date = new Date(now);
+    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd / HH:mm:ss");
+    String formatDate = dateFormat.format(date);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button1 = findViewById(R.id.button1);
 
-        button1.setOnClickListener( new View.OnClickListener()
-        {
+        //작성하기 버튼
+        button1 = findViewById(R.id.ButtonSubmit);
+
+        button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                Toast.makeText(getApplicationContext(), "인증되었습니다.",
+                Toast.makeText(getApplicationContext(), "명부가 작성되었습니다",
                         Toast.LENGTH_SHORT).show();
             }
         });
+
+        //시간 출력
+        Day=(TextView)findViewById(R.id.Day);
+        Day.setText(formatDate);
     }
 }
 
